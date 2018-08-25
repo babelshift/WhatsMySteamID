827c4457305904ce92224e66036f3e9769e5c82b
Justin Skiles <justin.d.skiles@gmail.com>
2018-08-25 18:41:26 -0400

    Converted to .NET Core

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SteamWebAPI2.Exceptions;
+using SteamWebAPI2.Models;
+using WhatsMySteamID.Models;
+
+namespace WhatsMySteamID.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ActionResult Index()
+        {
+            return View(new HomeIndexViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(HomeIndexViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string steamWebApiKey = Configuration.GetValue<string>("SteamWebApiKey");
+                    SteamId steamId = new SteamId(model.SearchString, steamWebApiKey);
+
+                    ulong steamId64 = steamId.To64Bit();
+
+                    model.AccountId = steamId.AccountId;
+                    model.AccountType = steamId.AccountType.ToString();
+                    model.Instance = steamId.Instance.ToString();
+                    model.CommunityProfileUrl = String.Format("http://steamcommunity.com/profiles/{0}", steamId64);
+                    model.LegacySteamId = steamId.ToLegacyFormat();
+                    model.ModernSteamId = steamId.ToModernFormat();
+                    model.SteamId64 = steamId64;
+                    model.Universe = steamId.Universe.ToString();
+                    model.IsValidSteamAccount = true;
+                    model.ResolvedBy = steamId.ResolvedFrom.ToString();
+                }
+                catch (VanityUrlNotResolvedException)
+                {
+                    ModelState.AddModelError("e", "Could not find any Steam Account for your entered value.");
+                }
+                catch (InvalidSteamCommunityUriException)
+                {
+                    ModelState.AddModelError("e", "The URL that you provided isn't a valid Steam Community URL.");
+                }
+                catch (SteamIdNotConstructedException)
+                {
+                    ModelState.AddModelError("e", "The Steam ID couldn't be constructed based on your input.");
+                }
+
+                return View(model);
+            }
+            else
+            {
+                return RedirectToRoute("index");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/HomeIndexViewModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WhatsMySteamID.Models
+{
+    public class HomeIndexViewModel
+    {
+        [Required(ErrorMessage = "You have to enter a non-empty value to search.")]
+        public string SearchString { get; set; }
+
+        public ulong SteamId64 { get; set; }
+        public string CommunityProfileUrl { get; set; }
+        public string LegacySteamId { get; set; }
+        public string ModernSteamId { get; set; }
+        public string Universe { get; set; }
+        public string AccountType { get; set; }
+        public string Instance { get; set; }
+        public uint AccountId { get; set; }
+        public bool IsValidSteamAccount { get; set; }
+        public string ResolvedBy { get; set; }
+    }
+}
